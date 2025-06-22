10a3ad63792a79bf7f409d0368954d36bed13fdd
unknown <bmp713@yahoo.com>
2025-06-22 14:59:23 -0700

    Frontend

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace Frontend.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? email { get; set; }
+        public string? password { get; set; }
+    }
+}
