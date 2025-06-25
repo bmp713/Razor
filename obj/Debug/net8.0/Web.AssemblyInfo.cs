46f469f3d2f31ef9f897455a32b075386f5efa8d
unknown <bmp713@yahoo.com>
2025-06-25 13:22:40 -0700

    Frontend social

--- obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b92f8530cd935d06fbad4551bec488bc51e295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef6c208befd17174ac8bc79eedcf483f74ed4f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
