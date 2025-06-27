033ed2b5a8f02466d732693fce7a95f9bc06092b
unknown <bmp713@yahoo.com>
2025-06-27 13:02:03 -0700

    Frontend UI

--- obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb492430c97cc3b4f883c2d6ae3448a97e7ecba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520f91c5b26a63c9675efc088203b1c6976f6bbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
