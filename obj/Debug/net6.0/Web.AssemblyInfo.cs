c122b5a59bfef6ff8de692af9be63ee56e3b00c1
unknown <bmp713@yahoo.com>
2025-06-28 10:17:10 -0700

    Frontend forms

--- obj/Debug/net6.0/Web.AssemblyInfo.cs
+++ obj/Debug/net6.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e06005906f5606a321e20ff9cd1e762e4331a93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+025c73f0b3947e72efc117ef401e53cb1028a926")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
