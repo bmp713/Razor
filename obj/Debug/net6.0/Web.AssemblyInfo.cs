36e8b84c15578278f5620f30a4d0e59f8ae7deb3
unknown <bmp713@yahoo.com>
2025-06-28 14:17:23 -0700

    Frontend README

--- obj/Debug/net6.0/Web.AssemblyInfo.cs
+++ obj/Debug/net6.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc516cee3db1db75f821ca29c6c08c5d5725b3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49791f89ac27d6ae046765862090dcc373a1eacc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
