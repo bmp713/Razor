d0d11b36c4c01f3b910a33f3ded54b43ad48b62c
unknown <bmp713@yahoo.com>
2025-06-29 14:06:51 -0700

    Frontend Themes

--- obj/Debug/net6.0/Web.AssemblyInfo.cs
+++ obj/Debug/net6.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c92a3dd55cd39e4c75d44448ec8b2f2a96a0ee6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edff97c67a7fec27c65e239203fbbf5fab39395c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
