58a529a4ea859e9d52ed54bcf3e7017a24d38d88
unknown <bmp713@yahoo.com>
2025-07-01 14:40:01 -0700

    Frontend Styles

--- obj/Debug/net6.0/Web.AssemblyInfo.cs
+++ obj/Debug/net6.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d765356190eb10c28f9bdef99b0e4f8d5074a42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ab818ec221e6419a5449d4f4438739f3af8dd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
